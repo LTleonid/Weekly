26177931e23fcd6e0072b0ac3a4fabde92e49209
nower <renottgod@gmail.com>
2025-06-27 00:26:30 +0700

    #

--- WinFormsApp3/Dungeons/Dungeons.cs
+++ WinFormsApp3/Dungeons/Dungeons.cs
@@ -30,9 +30,57 @@ namespace WinFormsApp3.Dungeons
 
 
         protected List<Control> gameObjects = new List<Control>();
-
-        public Dungeon(Player.Player player)
+        public Dungeon()
         {
+            dungeon = new Dungeon();
+            Mdown = false;
+            Mup = false;
+            Mleft = false;
+            Mright = false;
+            this.BackColor = Color.Green;
+
+            this.ClientSize = new Size(ViewportWidth, ViewportHeight);
+
+            Init();
+            this.KeyPreview = true;
+            int fps = 100;
+            System.Windows.Forms.Timer movementTimer = new System.Windows.Forms.Timer();
+            movementTimer.Interval = (1000 / fps);
+            movementTimer.Tick += MovementTimer_Tick;
+            movementTimer.Start();
+
+
+            System.Windows.Forms.Timer cameraTimer = new System.Windows.Forms.Timer();
+            cameraTimer.Interval = (1000 / fps);
+            cameraTimer.Tick += CameraTimer_Tick;
+            cameraTimer.Start();
+            this.ClientSize = new Size(600, 600);
+            this.Size = new Size(1200, 600);
+            this.MaximumSize = new Size(600, 600);
+            this.MinimumSize = new Size(600, 600);
+            this.Text = "В пещере...";
+            this.BackColor = Color.DarkGray;
+        }
+        private void CameraTimer_Tick(object sender, EventArgs e)
+        {
+            var playerWorldPos = (KeyValuePair<int, int>)Player.Sprite.Tag;
+            int targetX = playerWorldPos.Value - ((ViewportWidth - Player.Sprite.Size.Width) / 2);
+            int targetY = playerWorldPos.Key - ((ViewportHeight - Player.Sprite.Size.Height) / 2);
+
+            CameraX += (int)((targetX - CameraX) * 0.1);
+            CameraY += (int)((targetY - CameraY) * 0.1);
+
+            CameraX = Math.Max(CameraX, Math.Min(CameraX, WorldWidth - ViewportWidth));
+            CameraY = Math.Max(CameraY, Math.Min(CameraY, WorldHeight - ViewportHeight));
+            Player.Sprite.Location = new Point(playerWorldPos.Value - CameraX, playerWorldPos.Key - CameraY);
+            UpdateObjectsPosition();
+        }
+        private void UpdateObjectsPosition()
+        {
+            string text = "";
+            foreach (var obj in gameObjects)
+            {
+                KeyValuePair<int, int>? coords = (KeyValuePair<int, int>)obj.Tag;
 
 
             Init(player);
@@ -70,10 +118,43 @@ namespace WinFormsApp3.Dungeons
             base.OnKeyUp(e);
         }
 
-        public void Init(Player.Player player)
+        public void Init()
         {
 
-            Player = player;
+            Player = new Player.Player(ViewportWidth, ViewportHeight, Controls, gameObjects, "player", 100);
+            //Player.inventory.AddItemToInventory(Items.Sword());
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                PictureBox obj = new PictureBox();
+
+                obj.Location = new Point(rnd.Next(WorldWidth), rnd.Next(WorldHeight));
+                obj.Size = new Size(80, 80);
+                obj.Image = Properties.Resources.Tree;
+                obj.SizeMode = PictureBoxSizeMode.StretchImage;
+                obj.Name = "Tree";
+                obj.BringToFront();
+                obj.Tag = new KeyValuePair<int, int>(obj.Location.X, obj.Location.Y);
+                Controls.Add(obj);
+                gameObjects.Add(obj);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                PictureBox obj = new PictureBox();
+
+                obj.Location = new Point(rnd.Next(WorldWidth), rnd.Next(WorldHeight));
+                obj.Size = new Size(40, 80);
+                obj.Image = Properties.Resources.Door;
+                obj.SizeMode = PictureBoxSizeMode.StretchImage;
+                obj.Name = "Door";
+                obj.BringToFront();
+                obj.Tag = new KeyValuePair<int, int>(obj.Location.X, obj.Location.Y);
+                Controls.Add(obj);
+                gameObjects.Add(obj);
+            }
+
+            form.Show();
 
         }
     }
